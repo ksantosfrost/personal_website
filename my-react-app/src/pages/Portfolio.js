d1e47f0975f683e23cf5cc8fc512792b085f9ecf
Keanu <keanu.k.santos@gmail.com>
2025-02-24 17:07:04 -0800

    V.2

---
+++ my-react-app/src/pages/Portfolio.js
@@ -0,0 +1,15 @@
+// src/pages/Portfolio.js
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <section>
+      <h2>My Portfolio</h2>
+      <p>
+        Placeholder text for Portfolio. Showcase your projects here.
+      </p>
+    </section>
+  );
+}
+
+export default Portfolio;
